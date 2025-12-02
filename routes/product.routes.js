3a753e0fe146f248161dcdb97b2d9021384b610c
Confidence Kpodo <kaceno027@gmail.com>
2025-12-02 14:16:47 +0000

    update

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -27,7 +27,7 @@ router.get("/location/:location", findProductsByLocation);
 router.get("/user/:id", findProductsByUserId);
 router.get("/category/:category", findProductsByCategory);
 router.get("/:id", getProductById);
-router.put("/updateProduct/:id",upload.single('image'),authenticate, updateProduct);
+router.put("/updateProduct/:id",upload.array('images',5),authenticate, updateProduct);
 router.delete("/deleteProduct/:id", authenticate, deleteProduct);
 router.get("/name/:name", findProductsByName);
 
